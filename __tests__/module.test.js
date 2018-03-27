3f6566555c2c4cb9ea60bb9fdfa4c5058b9e144b
meriadec <meriadec.pillet@gmail.com>
2018-03-27 22:04:00 +0100

    Bind the `getDominantColor` function

--- __tests__/module.test.js
+++ __tests__/module.test.js
@@ -1,10 +1,13 @@
+const path = require('path')
+
 const colorThief = require('../lib/index.js')
 
 test('should expose a set of functions', () => {
   expect(colorThief.getDominantColor).toBeDefined()
 })
 
-test('should return a result', () => {
-  const res = colorThief.getDominantColor('/toto/titi.png')
-  expect(res).toBeDefined()
+test('should return a result', async () => {
+  const p = path.resolve(__dirname, 'images/gradient.jpg')
+  const color = await colorThief.getDominantColor(p)
+  expect(color).toBe('#d05e64')
 })
--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,12 +1,51 @@
+extern crate color_thief;
+extern crate image;
 #[macro_use]
 extern crate neon;
 
+use std::path;
+use color_thief::{Color, ColorFormat};
+use neon::mem::Handle;
 use neon::vm::{Call, JsResult};
-use neon::js::JsString;
+use neon::js::{JsFunction, JsNull, JsString, JsUndefined};
 
-fn get_dominant_color(call: Call) -> JsResult<JsString> {
-    let scope = call.scope;
-    Ok(JsString::new(scope, "hello node").unwrap())
+fn find_color(t: image::ColorType) -> ColorFormat {
+    match t {
+        image::ColorType::RGB(8) => ColorFormat::Rgb,
+        image::ColorType::RGBA(8) => ColorFormat::Rgba,
+        _ => unreachable!(),
+    }
+}
+
+fn get_dominant_color(call: Call) -> JsResult<JsUndefined> {
+    let f = call.arguments
+        .require(call.scope, 0)?
+        .check::<JsString>()?
+        .value();
+
+    let img = image::open(&path::Path::new(&f)).unwrap();
+    let color_type = find_color(img.color());
+    let colors = color_thief::get_palette(&img.raw_pixels(), color_type, 10, 10).unwrap();
+    let Color {
+        r: _r,
+        g: _g,
+        b: _b,
+    } = colors[0];
+
+    let mut rr: String = "".to_owned();
+    rr.push_str("#");
+    rr.push_str(&format!("{:x}", _r));
+    rr.push_str(&format!("{:x}", _g));
+    rr.push_str(&format!("{:x}", _b));
+
+    let callback = call.arguments.get(call.scope, 1).unwrap();
+    let file_name = JsString::new(call.scope, &rr).unwrap();
+
+    if let Some(function) = callback.downcast::<JsFunction>() {
+        let args: Vec<Handle<JsString>> = vec![file_name];
+        let _ = function.call(call.scope, JsNull::new(), args);
+    }
+    Ok(JsUndefined::new())
 }
 
 register_module!(m, { m.export("getDominantColor", get_dominant_color) });
